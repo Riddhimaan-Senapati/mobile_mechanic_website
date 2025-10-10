5e8550fc4c15e14c61cc00e956497ce800350682
Ava <axfu@umass.edu>
2025-10-09 20:57:30 -0400

    Finished map background + zip code overlay

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -5,6 +5,8 @@ import NavBar from "./NavBar";
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button";
 import { Label } from "@radix-ui/react-label";
+import { Card } from "@/components/ui/card";
+import { CardContent } from "@/components/ui/card";
 
 //checking branch if it's working
 
@@ -12,16 +14,31 @@ export default function Home() {
   return (
     <div>
       <NavBar />
+      <section className="mx-auto max-w-8xl px-4 py-8 h-1/2">
+        <div className="relative h-[50vh] w-full overflow-hidden rounded-md">
+          <iframe
+            src="https://www.openstreetmap.org/export/embed.html?bbox=-72.69790649414064,42.27680072484333,-72.3992156982422,42.45816732375829&layer=mapnik"
+            className="h-full w-full border rounded-md"
+            title="Map of Amherst"
+          />
 
-      <div>
-        <h1 className="text-lg"> Are we in your area? </h1>
-        <div className="flex w-full max-w-sm items-center gap-2">
-          <Input type="text" pattern="[0-9]{5}" id="zipcode" placeholder="Five-digit zip code" />
-          <Button type="submit" variant="outline">
-            Go
-          </Button>
+          <div className="absolute inset-x-0 bottom-0 z-10 p-2">
+            <Card className="mx-auto max-w-lg border-white/30 bg-white/80 backdrop-blur supports-[backdrop-filter]:bg-white/50 flex items-center">
+              <CardContent className="">
+                <h1 className="text-lg text-center"> Are we in your area? </h1>
+
+                <div className="flex w-full max-w-sm items-center gap-2">
+              <Input type="text" pattern="[0-9]{5}" id="zipcode" placeholder="Five-digit zip code" />
+              <Button type="submit" variant="outline">
+                Go
+              </Button>
+                </div>
+              </CardContent>
+            </Card>
+          </div> 
+
         </div>
-      </div>
+      </section>
       
     </div>
   );
