354c46055e0068dd4a49effc0402d6584761ed9a
Ava <axfu@umass.edu>
2025-10-13 13:29:45 -0400

    Formatting with Prettier

--- frontend/app/Footer.tsx
+++ frontend/app/Footer.tsx
@@ -1,25 +1,33 @@
-'use client'
+"use client";
 import { Separator } from "@radix-ui/react-separator";
 
-export default function Footer(){
-    return(
-        <footer>
-            {/* <Separator className="bg-gray-200 dark:bg-gray-800 h-px w-full" orientation="horizontal"/> */}
-            <div className="mx-auto flex w-full justify-center p-4">
-                <div className="flex object-center gap-3">
-                    <div>
-                    <p className="text-sm text-muted-foreground">Contact Us</p>
-                    </div>
-                    <Separator className="bg-gray-200 dark:bg-gray-800 w-px h-6"orientation="vertical"/>
-                    <div>
-                    <p className="text-sm text-muted-foreground">123-456-7890</p>
-                    </div>
-                    <Separator className="bg-gray-200 dark:bg-gray-800 w-px h-6"orientation="vertical"/>
-                    <div>
-                    <p className="text-sm text-muted-foreground">help@mobilemechanic.com</p>
-                    </div>
-                </div>
-            </div>
-        </footer>
-    );
+export default function Footer() {
+  return (
+    <footer>
+      {/* <Separator className="bg-gray-200 dark:bg-gray-800 h-px w-full" orientation="horizontal"/> */}
+      <div className="mx-auto flex w-full justify-center p-4">
+        <div className="flex object-center gap-3">
+          <div>
+            <p className="text-sm text-muted-foreground">Contact Us</p>
+          </div>
+          <Separator
+            className="bg-gray-200 dark:bg-gray-800 w-px h-6"
+            orientation="vertical"
+          />
+          <div>
+            <p className="text-sm text-muted-foreground">123-456-7890</p>
+          </div>
+          <Separator
+            className="bg-gray-200 dark:bg-gray-800 w-px h-6"
+            orientation="vertical"
+          />
+          <div>
+            <p className="text-sm text-muted-foreground">
+              help@mobilemechanic.com
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
