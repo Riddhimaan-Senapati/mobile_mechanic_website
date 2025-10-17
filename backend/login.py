a1361f282f8142de6fb5f679b02e1fc88fe56c56
Riddhimaan-Senapati <rsenapati@umass.edu>
2025-10-17 12:07:54 -0400

    added workflows to check linting during PRs

--- backend/login.py
+++ backend/login.py
@@ -11,7 +11,7 @@ DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
 
-url = f"postgresql://postgres:password123@db.lperpdujkzvsotsthwtd.supabase.co:5432/postgres"
+url = "postgresql://postgres:password123@db.lperpdujkzvsotsthwtd.supabase.co:5432/postgres"
 
 engine = create_engine(url, connect_args={"sslmode": "require"})
 
