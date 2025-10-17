a2d4e3f26497767c31d2714abf4a5ff0da5db994
Ava <axfu@umass.edu>
2025-10-17 12:21:07 -0400

    Formatting with Prettier

--- frontend/app/billing/page.tsx
+++ frontend/app/billing/page.tsx
@@ -1,5 +1,5 @@
-'use client' 
-import NavBar from "../NavBar"; 
+"use client";
+import NavBar from "../NavBar";
 import {
   Table,
   TableBody,
@@ -8,36 +8,34 @@ import {
   TableHead,
   TableHeader,
   TableRow,
-} from "@/components/ui/table"
+} from "@/components/ui/table";
 
 export default function Billing() {
-    return(
-        <div>
-            <NavBar /> 
+  return (
+    <div>
+      <NavBar />
 
-            <h1 className="text-lg">
-                Billing
-            </h1>
+      <h1 className="text-lg">Billing</h1>
 
-            <Table>
-                <TableCaption>A list of your recent invoices.</TableCaption>
-                <TableHeader>
-                    <TableRow>
-                    <TableHead className="w-[100px]">Invoice</TableHead>
-                    <TableHead>Status</TableHead>
-                    <TableHead>Method</TableHead>
-                    <TableHead className="text-right">Amount</TableHead>
-                    </TableRow>
-                </TableHeader>
-                <TableBody>
-                    <TableRow>
-                    <TableCell className="font-medium">INV001</TableCell>
-                    <TableCell>Paid</TableCell>
-                    <TableCell>Credit Card</TableCell>
-                    <TableCell className="text-right">$250.00</TableCell>
-                    </TableRow>
-                </TableBody>
-            </Table>
-        </div> 
-    );
+      <Table>
+        <TableCaption>A list of your recent invoices.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">Invoice</TableHead>
+            <TableHead>Status</TableHead>
+            <TableHead>Method</TableHead>
+            <TableHead className="text-right">Amount</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell className="font-medium">INV001</TableCell>
+            <TableCell>Paid</TableCell>
+            <TableCell>Credit Card</TableCell>
+            <TableCell className="text-right">$250.00</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </div>
+  );
 }
--- frontend/app/customer_landing/page.tsx
+++ frontend/app/customer_landing/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import { use } from "react";
 import NavBar from "../NavBar";
 import { useRouter } from "next/navigation";
@@ -6,51 +6,57 @@ import { ButtonGroup } from "@/components/ui/button-group";
 import { Button } from "@/components/ui/button";
 import { Book } from "lucide-react";
 
-function BookingButton(){
-    const router = useRouter(); 
-    const goto_booking = () => {
-        router.push("/create_booking"); 
-    }
+function BookingButton() {
+  const router = useRouter();
+  const goto_booking = () => {
+    router.push("/create_booking");
+  };
 
-    return(
-        <Button variant="outline" onClick={goto_booking}>Create Booking</Button>
-    );
+  return (
+    <Button variant="outline" onClick={goto_booking}>
+      Create Booking
+    </Button>
+  );
 }
 
-function MessagingButton(){
-    const router = useRouter(); 
-    const goto_messaging = () => {
-        router.push("/messaging"); 
-    }
+function MessagingButton() {
+  const router = useRouter();
+  const goto_messaging = () => {
+    router.push("/messaging");
+  };
 
-    return(
-        <Button variant="outline" onClick={goto_messaging}>Messaging</Button>
-    );
+  return (
+    <Button variant="outline" onClick={goto_messaging}>
+      Messaging
+    </Button>
+  );
 }
 
-function BillingButton(){
-    const router = useRouter(); 
-    const goto_billing = () => {
-        router.push("/billing"); 
-    }
+function BillingButton() {
+  const router = useRouter();
+  const goto_billing = () => {
+    router.push("/billing");
+  };
 
-    return(
-        <Button variant="outline" onClick={goto_billing}>Billing</Button>
-    );
+  return (
+    <Button variant="outline" onClick={goto_billing}>
+      Billing
+    </Button>
+  );
 }
 
-export default function Customer_Landing(){
-    return(
-        <div>
-            <NavBar/>
+export default function Customer_Landing() {
+  return (
+    <div>
+      <NavBar />
 
-            <h1 className="text-lg">Account Info</h1>
+      <h1 className="text-lg">Account Info</h1>
 
-            <ButtonGroup>
-                <MessagingButton />
-                <BookingButton /> 
-                <BillingButton /> 
-            </ButtonGroup>
-        </div>
-    )
+      <ButtonGroup>
+        <MessagingButton />
+        <BookingButton />
+        <BillingButton />
+      </ButtonGroup>
+    </div>
+  );
 }
