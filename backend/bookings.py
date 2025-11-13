e44aefe71011805f674ecf128a61f4ba68c8a948
ZhiyangWen <144734971+ZhiyangWen@users.noreply.github.com>
2025-11-13 10:24:31 -0500

    Add final booking tests and update booking.py for test_user integration

--- backend/bookings.py
+++ backend/bookings.py
@@ -1,10 +1,12 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Query
 from sqlalchemy import create_engine, text
 from pydantic import BaseModel, EmailStr, HttpUrl
 from dotenv import load_dotenv
 import os
 from pathlib import Path
 from datetime import datetime
+from typing import Optional
+from datetime import datetime as dt
 
 load_dotenv(dotenv_path=Path(__file__).parent / ".env")
 
@@ -21,6 +23,10 @@ engine = create_engine(DATABASE_URL, connect_args={"sslmode": "require"})
 
 
 app = FastAPI()
+
+def get_db():
+    with engine.begin() as conn:
+        yield conn
 
 class Booking(BaseModel):
     email: EmailStr
@@ -43,24 +49,34 @@ class EditBooking(BaseModel):
     year: str | None = None
     address: str | None = None
     description: str | None = None
-    datetime: datetime
+    datetime: Optional[dt] = None
+
+def get_table_name(test:bool):
+    return '"Test_User"' if test else '"BookingInfo"'
 
 @app.post("/add-new-booking")
-def new_booking(booking: Booking):
-    query = text(""" INSERT INTO "BookingInfo" (email, make, model, year, address, description, datetime)
+def new_booking(booking: Booking, test: bool = Query(default=False)):
+    table = get_table_name(test)
+    '''query = text(""" INSERT INTO "BookingInfo" (email, make, model, year, address, description, datetime)
                  VALUES (:email, :make, :model, :year, :address, :description, :datetime) 
-                 RETURNING id; """)
+                 RETURNING id; """)'''
+    query = text(f"""
+        INSERT INTO {table} (email, make, model, year, address, description, datetime)
+        VALUES (:email, :make, :model, :year, :address, :description, :datetime)
+        RETURNING id;
+    """)
     try:
         with engine.begin() as conn:
             res = conn.execute(query, booking.model_dump())
             booking_id = res.scalar()
-        return{"message":"New booking added!"}
+        return{"message":f"New booking added into {table}"}
     except Exception as e:
         raise HTTPException(status_code=404, detail=f"Error adding booking: {str(e)}")
 
 @app.delete("/delete-booking")
-def delete_booking(email: EmailStr, date_time: datetime):
-    query = text(""" DELETE from "BookingInfo" WHERE email = :email AND datetime = :datetime
+def delete_booking(email: EmailStr, date_time: datetime, test:bool = Query(default=False)):
+    table = get_table_name(test)
+    query = text(f""" DELETE from {table} WHERE email = :email AND datetime = :datetime
                  RETURNING id; """)
     try:
         with engine.begin() as conn:
@@ -68,13 +84,16 @@ def delete_booking(email: EmailStr, date_time: datetime):
             del_id = res.scalar()
             if not del_id:
                 raise HTTPException(status_code=404, detail="No booking found")
-        return {"message": "Booking deleted successfully"}
+        return {"message": f"Booking deleted successfully from {table}"}
+    except HTTPException:
+        raise
     except Exception as e:
         raise HTTPException(status_code=400, detail = f"Error deleting booking: {str(e)}")
 
 @app.get("/get-bookings")  
-def get_all_bookings():
-    query = text(""" SELECT * FROM "BookingInfo" ORDER BY datetime DESC; """)
+def get_all_bookings(test:bool = Query(default=False)):
+    table = get_table_name(test)
+    query = text(f""" SELECT * FROM {table} ORDER BY datetime DESC; """)
     try:
         with engine.begin() as conn:
             res = conn.execute(query)
@@ -84,19 +103,20 @@ def get_all_bookings():
         raise HTTPException(status_code=400, detail = f"Error fetch bookings: {str(e)}")
     
 @app.put("/edit-booking")
-def edit_booking(edit: EditBooking):
-    fields = {}
+def edit_booking(edit: EditBooking, test:bool = Query(default=False)):
+    table = get_table_name(test)
+    fields = []
     params = {"id":edit.id}
-    for key, value in edit.model_dump():
+    for key, value in edit.model_dump(exclude_unset=True).items():
         if (key != "id"):
             fields.append(f'{key} = :{key}')
             params[key] = value
     if (len(fields) == 0):
         raise HTTPException(status_code=400, detail="No fields provided for update")
     
-    query = text('''
-        UPDATE "BookingInfo"
-        SET {", ".join(fields)}
+    query = text(f'''
+        UPDATE {table}
+        SET {",".join(fields)}
         WHERE id = :id
         RETURNING id;
     ''')
@@ -107,9 +127,10 @@ def edit_booking(edit: EditBooking):
             if not updated_ornot:
                 raise HTTPException(status_code=404, detail="Booking not found")
         return {"message": "Booking updated successfully"}
+    except HTTPException:
+        raise 
     except Exception as e:
         raise HTTPException(status_code=400, detail=f"Error updating booking: {str(e)}")
-
 
 
     
